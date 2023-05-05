7c0caa211462b8bfa338d9d5b35fa39e92e12ce9
fremovro <fremov.ro@mail.ru>
2023-05-05 19:18:08 +0500

    Commit motherfucka

--- DPF C-sh/Methods/AccessoryMethods.cs
+++ DPF C-sh/Methods/AccessoryMethods.cs
@@ -152,25 +152,23 @@ namespace DPF_C_sh.Methods
         {
             var musIntervals = new Dictionary<double, Color>()
             {
-                { 0.89, Color.Pink }, // Большая секунда
-                { 0.84, Color.Blue }, // Малая терция
-                { 0.79, Color.Red }, // Большая терция
-                { 0.75, Color.Yellow }, // Кварта (чистая)
-                { 0.67, Color.Gray }, // Квинта (чистая)
-                { 0.63, Color.Orange }, // Малая секста
-                { 0.6, Color.Green }, // Большая секста
-                { 0.56, Color.YellowGreen }, // Маля септима
-                { 0.53, Color.OrangeRed }, // Большая септима
-                { 0.5, Color.BlueViolet } // Октава
+                { 0.89, Color.FromArgb(191, 191, 191) }, // Большая секунда
+                { 0.84, Color.FromArgb(102, 255, 153) }, // Малая терция
+                { 0.79, Color.FromArgb(102, 255, 153) }, // Большая терция
+                { 0.75, Color.FromArgb(102, 255, 255) }, // Кварта (чистая)
+                { 0.67, Color.FromArgb(255, 255, 153) }, // Квинта (чистая)
+                { 0.63, Color.FromArgb(255, 153, 255) }, // Малая секста
+                { 0.6, Color.FromArgb(102, 255, 153) }, // Большая секста
+                { 0.56, Color.FromArgb(255, 153, 153) }, // Маля септима
+                { 0.53, Color.FromArgb(153, 204, 255) }, // Большая септима
+                { 0.5, Color.FromArgb(191, 191, 191) } // Октава
             };
-
-            var emotionStr = new List<string>() { "Радость", "Страх", "Отвращение", "Грусть" };
-
+            var emotionStr = new List<string>() { "Радость", "Страх", "Отвращение" };
             var emotionMusInt = new List<List<double>>() {
-                new List<double> {0.5, 0.67, 0.75 },
+                new List<double> {0.79, 0.67, 0.75, 0.6, 0.84},
                 new List<double> {0.53, 0.75, 0.63 },
-                new List<double> {0.63, 0.67, 0.56 },
-                new List<double> {0.84, 0.5, 0.6 }
+                new List<double> {0.63, 0.67, 0.56 }
+                //new List<double> {0.84, 0.5, 0.6 }
             };
 
             // Создаём объект - экземпляр нашего приложения
@@ -184,48 +182,126 @@ namespace DPF_C_sh.Methods
             workBook = excelApp.Workbooks.Add();
             workSheet = (Microsoft.Office.Interop.Excel.Worksheet)workBook.Worksheets.get_Item(1);
 
-            double[][] output = new double[dataContext.requencyRatios.ToArray().Length][];
+            var rowCount = dataContext.requencyRatios.Count();
+            var columnCount = dataContext.requencyRatios.First().Value.Count();
 
-            foreach (var el in dataContext.wavFiles)
-                output[el.Key] = new double[] { el.Value.emotionNum };
-
-            int index = 1;
-            foreach (var el in dataContext.requencyRatios)
+            // Заполнение шапки
+            for (int i = 0, j = 0, k = 0, upIndent = 1, leftIdent = 2; j < columnCount + 4; j++)
             {
-                if (index == 1)
+                if(j<columnCount)
+                    workSheet.Cells[i + upIndent, j + leftIdent] = $"X{j + 1}";
+                if(j==columnCount)
+                    workSheet.Cells[i + upIndent, j + leftIdent] = $"Y1";
+                if (j > columnCount)
                 {
-                    for (var i = 0; i < el.Value.Count; i++)
-                        workSheet.Cells[index, i + 2] = $"X{i + 1}";
-                    workSheet.Cells[index, el.Value.Count + 2] = $"Y1";
-
-                    for (int i = el.Value.Count + 3, j = 0; j < 4; i++, j++)
-                        workSheet.Cells[index, i] = emotionStr[j];
-
-                    index++;
+                    workSheet.Cells[i + upIndent, j + leftIdent] = emotionStr[k];
+                    k++;
                 }
-                el.Value.Sort();
-                workSheet.Cells[index, 1] = dataContext.wavFiles
-                    .Where(e => e.Key == el.Key).FirstOrDefault().Value.fileName;
-                for (var i = 0; i < el.Value.Count; i++)
+            }
+
+            // Заполнение названий
+            for (int i = 0, j = 0, upIndent = 2, leftIdent = 1; i < rowCount; i++)
+                workSheet.Cells[i + upIndent, j + leftIdent] = dataContext.wavFiles
+                    .Where(e => e.Key == dataContext.requencyRatios.ToArray()[i].Key).FirstOrDefault().Value.fileName;
+
+            // Заполнение значений
+            for (int i = 0, upIndent = 2, leftIdent = 2; i < rowCount; i++)
+                for (int j = 0; j < columnCount; j++)
                 {
-                    workSheet.Cells[index, i + 2] = el.Value[i];
+                    dataContext.requencyRatios.ToArray()[i].Value.Sort();
                     foreach (var interval in musIntervals)
-                        if (interval.Key == el.Value[i])
-                            workSheet.Cells[index, i + 2].Interior.Color = interval.Value;
+                        if (interval.Key == dataContext.requencyRatios.ToArray()[i].Value[j])
+                            workSheet.Cells[i + upIndent, j + leftIdent].Interior.Color = interval.Value;
+                    workSheet.Cells[i + upIndent, j + leftIdent] = dataContext.requencyRatios.ToArray()[i].Value[j];
                 }
-                workSheet.Cells[index, el.Value.Count + 2] = output[el.Key][0];
 
-                for (int i = el.Value.Count + 3, j = 0; j < 4; i++, j++)
+            // Заполнение предполагаемых результатов
+            for (int i = 0, j = 0, upIndent = 2, leftIdent = 2 + columnCount; i < rowCount; i++)
+                workSheet.Cells[i + upIndent, j + leftIdent] = dataContext.wavFiles
+                    .Where(e => e.Key == dataContext.requencyRatios.ToArray()[i].Key).FirstOrDefault().Value.emotionNum;
+
+            //Заполнение итогов по данному набору параметров
+            var correctCount = 0;
+            for (int i = 0, upIndent = 2, leftIdent = 3 + columnCount; i < rowCount; i++)
+            {
+                var maxCount = 0;
+                var maxCountIndex = 0;
+                for (int j = 0; j < emotionMusInt.Count(); j++)
                 {
                     var count = 0;
-                    for(var k = 0; k < 3; k++)
-                        if (el.Value.Contains(emotionMusInt[j][k]))
+                    for (int k = 0; k < emotionMusInt[j].Count(); k++)
+                        if (dataContext.requencyRatios.ToArray()[i].Value.Contains(emotionMusInt[j][k]))
                             count++;
-                    workSheet.Cells[index, i] = count;
+                    if(count>maxCount)
+                    {
+                        maxCount = count;
+                        maxCountIndex = j + 1;
+                    }
+                    workSheet.Cells[i + upIndent, j + leftIdent] = count;
                 }
-
-                index++;
+                workSheet.Cells[i + upIndent, emotionMusInt.Count() + leftIdent] = maxCountIndex;
+                if (maxCountIndex == dataContext.wavFiles
+                    .Where(e => e.Key == dataContext.requencyRatios.ToArray()[i].Key).FirstOrDefault().Value.emotionNum)
+                    {
+                        workSheet.Cells[i + upIndent, emotionMusInt.Count() + leftIdent].Interior.Color = Color.FromArgb(169, 208, 142);
+                        correctCount++;
+                    }
+                else
+                    workSheet.Cells[i + upIndent, emotionMusInt.Count() + leftIdent].Interior.Color = Color.FromArgb(244, 176, 132);
             }
+            workSheet.Cells[1, 1] = correctCount;
+
+            //double[][] output = new double[dataContext.requencyRatios.ToArray().Length][];
+
+            //foreach (var el in dataContext.wavFiles)
+            //    output[el.Key] = new double[] { el.Value.emotionNum };
+
+            //int index = 1;
+            //foreach (var el in dataContext.requencyRatios)
+            //{
+            //    if (index == 1)
+            //    {
+            //        for (var i = 0; i < el.Value.Count; i++)
+            //            workSheet.Cells[index, i + 2] = $"X{i + 1}";
+            //        workSheet.Cells[index, el.Value.Count + 2] = $"Y1";
+
+            //        for (int i = el.Value.Count + 3, j = 0; j < 4; i++, j++)
+            //            workSheet.Cells[index, i] = emotionStr[j];
+
+            //        index++;
+            //    }
+            //    el.Value.Sort();
+            //    workSheet.Cells[index, 1] = dataContext.wavFiles
+            //        .Where(e => e.Key == el.Key).FirstOrDefault().Value.fileName;
+            //    for (var i = 0; i < el.Value.Count; i++)
+            //    {
+            //        workSheet.Cells[index, i + 2] = el.Value[i];
+            //        foreach (var interval in musIntervals)
+            //            if (interval.Key == el.Value[i])
+            //                workSheet.Cells[index, i + 2].Interior.Color = interval.Value;
+            //    }
+            //    workSheet.Cells[index, el.Value.Count + 2] = output[el.Key][0];
+
+            //    var lasti = 0;
+            //    var maxcount = 0;
+            //    var maxi = 0;
+            //    for (int i = el.Value.Count + 3, j = 0; j < emotionMusInt.Count(); i++, j++)
+            //    {
+            //        var count = 0;
+            //        for(var k = 0; k < 3; k++)
+            //            if (el.Value.Contains(emotionMusInt[j][k]))
+            //                count++;
+            //        workSheet.Cells[index, i] = count;
+            //        if (count > maxcount) 
+            //        { 
+            //            maxcount = count; maxi = j; 
+            //        }
+            //        lasti = i; 
+            //    }
+            //    workSheet.Cells[index, lasti + 1] = maxi + 1;
+
+            //    index++;
+            //}
             // Открываем созданный excel-файл
             excelApp.Visible = true;
             excelApp.UserControl = true;
